d3e613600958749153d27917d756f02bc76ecf60
akar867 <kararnab99@gmail.com>
2023-06-24 22:13:44 +0530

    mongodb_cloud_link

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // connecting mongoose to its default server and ecommerceDB
 //when connecting to database point it to mongodb://127.0.0.1:27017/products
-mongoose.connect('mongodb://127.0.0.1:27017/products', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true
 });
 
