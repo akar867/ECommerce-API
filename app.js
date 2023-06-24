d274a50a68a8f149d7f73bb27507a42c0c9ea8f3
akar867 <kararnab99@gmail.com>
2023-06-24 16:59:55 +0530

    whole commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const dotEnv = require('dotenv');
+dotEnv.config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./config/mongoose');
+const {MONGODB_URL} = process.env;
+
+// initializing express
+const app = express();
+
+// using body parser to parse over the request body
+app.use(bodyParser.urlencoded({extended: true}));
+
+// using routes
+app.use('/products', require('./routes/products'));
+
+// starting the server
+app.listen(3000, function(){
+    console.log('API is live on http://localhost:3000/products');
+});
